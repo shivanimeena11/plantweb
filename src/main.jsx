0e0978ec9e7b0f652e3512cbb98c675310e5e27f
shivanimeena11 <shivanimeena35689@gmail.com>
2026-02-20 15:31:27 +0530

    edit main page

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { CartProvider } from "./context/CartContext";
-import "./index.css"; // your tailwind
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
